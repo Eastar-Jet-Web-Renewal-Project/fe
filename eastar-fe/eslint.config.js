ae3562d0cf9f1881b2f475b30626ed242fbebe33
nakyoung98 <jay3925@cau.ac.kr>
2024-08-12 03:00:41 +0900

    Add: eslint config에 플러그인 추가

--- eastar-fe/eslint.config.js
+++ eastar-fe/eslint.config.js
@@ -4,11 +4,30 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-config-prettier";
 
+import { projectStructurePlugin } from "eslint-plugin-project-structure";
+import { folderStructureConfig } from "./folderStructure.mjs";
+import { version } from "typescript";
+
 export default [
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   eslintConfigPrettier,
+  {
+    plugins: {
+      "project-structure": projectStructurePlugin,
+    },
+    rules: {
+      "project-structure/folder-structure": ["error", folderStructureConfig],
+    },
+  },
 ];
