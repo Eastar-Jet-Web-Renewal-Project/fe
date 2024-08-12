2fffef6957bacc58ef2394a71507ee29772a3eca
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-08-12 17:46:55 +0900

    Feat: ESLint 폴더 및 파일 관련 규칙 추가 (#8)
    * Chore: eslint-plugin-project-structure 라이브러리 설치
    
    * Chore: eslint-plugin-react 라이브러리 설치
    
    * Add: eslint config에 플러그인 추가
    
    * Feat: 폴더, 파일 네이밍 컨벤션 검사 추가
    
    * Fix: 네이밍 컨벤션 수정
    
    재귀, 빈 폴더 인식 등 오류 다수 수정
    
    * Chore: build 시 lint 검사 포함
    
    * Chore: git 대소문자 구별하도록 설정
    
    * Fix: convention 맞춰 폴더 네이밍 변경
    
    camelCase -> PascalCase

--- eastar-fe/eslint.config.js
+++ eastar-fe/eslint.config.js
@@ -4,11 +4,51 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-config-prettier";
 
+import {
+  projectStructureParser,
+  projectStructurePlugin,
+} from "eslint-plugin-project-structure";
+import { folderStructureConfig } from "./folderStructure.mjs";
+
 export default [
+  { ignores: ["dist/**", "node_modules/**", "public/**"] },
+  {
+    settings: {
+      react: {
+        version: "detect", // Automatically detect React version
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   eslintConfigPrettier,
+  {
+    files: [
+      "**/*.{js,mjs,cjs,ts,jsx,tsx}",
+      // 확장자가 없는 파일
+      // [참고] https://eslint.org/docs/latest/use/configure/configuration-files#specifying-files-without-extension
+      "**/!(*.*)",
+      "**/*.json",
+      // 빈 폴더 인식
+      "**/.gitkeep",
+    ],
+    languageOptions: {
+      parser: projectStructureParser,
+    },
+    plugins: {
+      "project-structure": projectStructurePlugin,
+    },
+    rules: {
+      "project-structure/folder-structure": ["error", folderStructureConfig],
+    },
+  },
 ];
---
+++ eastar-fe/folderStructure.mjs
@@ -0,0 +1,95 @@
+import {createFolderStructure} from "eslint-plugin-project-structure";
+
+export const folderStructureConfig = createFolderStructure({
+  structure: {
+    children: [
+      {
+        name: "*",
+      },
+      {
+        name: "src",
+        children: [
+          {name: "App.tsx"},
+          {name: "main.tsx"},
+          {name: "vite-env.d.ts"},
+          {
+            name: "@commons",
+            children: [
+              {name: "components", ruleId: "componentRule"},
+              {name: "constants", ruleId: "tsRule"},
+              {name: "hooks", ruleId: "hooksRule"},
+              {name: "layouts", ruleId: "componentRule"},
+              {name: "routes", ruleId: "componentRule"},
+              {name: "services", ruleId: "tsRule"},
+              {name: "stores", ruleId: "tsRule"},
+              {name: "types", ruleId: "tsRule"},
+              {name: "utilities", ruleId: "tsRule"},
+            ],
+          },
+          {name: "assets", children: [{name: "*"}]},
+          {
+            name: "domains",
+            children: [
+              {
+                name: "*",
+                children: [
+                  {name: "apis", ruleId: "tsRule"},
+                  {name: "components", ruleId: "componentRule"},
+                  {name: "hooks", ruleId: "hooksRule"},
+                  {name: "layouts", ruleId: "componentRule"},
+                  {name: "pages", ruleId: "componentRule"},
+                  {name: "types", ruleId: "tsRule"},
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  rules: {
+    hooksRule: {
+      children: [
+        {name: ".gitkeep"},
+        {name: "use{PascalCase}.ts"},
+        {
+          name: "use{PascalCase}",
+          children: [
+            {
+              name: "use{PascalCase}",
+              children: [
+                {name: ".gitkeep"},
+                {name: "use{PascalCase}.ts"},
+                {name: "use{PascalCase}", ruleId: "hooksRule"},
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    componentRule: {
+      children: [
+        {name: ".gitkeep"},
+        {name: "{PascalCase}(.styled)?.tsx"},
+        {
+          name: "{PascalCase}",
+          children: [
+            {name: ".gitkeep"},
+            {name: "{PascalCase}(.styled)?.tsx"},
+            {name: "{PascalCase}", ruleId: "componentRule"},
+          ],
+        },
+      ],
+    },
+    tsRule: {
+      children: [
+        {name: ".gitkeep"},
+        {name: "{camelCase}(.*)?.ts"},
+        {
+          name: "{camelCase}",
+          children: [{name: ".gitkeep"}, {name: "{camelCase}(.*)?.ts"}, {name: "{camelCase}", ruleId: "tsRule"}],
+        },
+      ],
+    },
+  },
+});
