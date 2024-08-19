8c18794d446becb5f320e0415e154888a1b8cacd
nakyoung98 <jay3925@cau.ac.kr>
2024-08-19 18:25:16 +0900

    Feat: ColorBox 고도화
    Box 색상에 따른 문구 색 동적 변경
    모든 글씨 길이 커버 위한 가로 너비 변경

--- eastar-fe/src/domains/design/components/ColorBox.tsx
+++ eastar-fe/src/domains/design/components/ColorBox.tsx
@@ -1,4 +1,6 @@
+import { getLuminance, setLightness } from "polished";
 import styled from "styled-components";
+import { color } from "../../../styles/color.styles";
 
 type ColorBoxProps = {
   name: string;
@@ -7,16 +9,26 @@ type ColorBoxProps = {
 
 export const ColorBox = styled.div<ColorBoxProps>`
   position: relative;
-  width: 120px;
+  width: 100%;
   height: 120px;
 
   background-color: ${(props) => props.color};
 
-  ::before {
-    content: ${(props) => props.name};
+  &:after {
+    content: "${(props) => props.name}";
+
     position: absolute;
     right: 10px;
     bottom: 10px;
+    z-index: 1;
+
+    color: ${
+      (props) =>
+        getLuminance(props.color) > 0.3
+          ? color.black // 어두운 텍스트
+          : color.white // 밝은 텍스트
+    };
+
     text-align: end;
   }
 `;
