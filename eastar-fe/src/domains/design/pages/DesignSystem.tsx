4ea245a4c7eec99901ce0eb117b2abf0753ac74d
nakyoung98 <jay3925@cau.ac.kr>
2024-08-19 18:42:31 +0900

    Feat: DesignSystem 페이지 구현

--- eastar-fe/src/domains/design/pages/DesignSystem.tsx
+++ eastar-fe/src/domains/design/pages/DesignSystem.tsx
@@ -1,3 +1,24 @@
+import styled from "styled-components";
+import { color } from "../../../styles/color.styles";
+import theme from "../../../styles/Theme/light.theme";
+import { ColorBox } from "../components/ColorBox";
+
+const DesignSystemWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
+
 export default function DesignSystem() {
-  return <div>DesignSystem</div>;
+  return (
+    <>
+      <h1>Design System</h1>
+      <p>현재 테마는 Light 입니다</p>
+      <DesignSystemWrapper>
+        {Object.entries(theme.colors).map(([name, value]) => (
+          <ColorBox key={name} name={name} color={value} />
+        ))}
+      </DesignSystemWrapper>
+    </>
+  );
 }
