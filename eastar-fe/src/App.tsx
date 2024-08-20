462bcb5191f998250d24219fddf644eccc8ca3a1
nakyoung98 <jay3925@cau.ac.kr>
2024-08-20 18:31:48 +0900

    Test: 전화번호 인증 구현 테스트용 임시 코드 구현

--- eastar-fe/src/App.tsx
+++ eastar-fe/src/App.tsx
@@ -1,5 +1,78 @@
+import { useRef, useState } from "react";
+import useReCaptcha from "./@commons/hooks/useAuth/useRecaptcha";
+import { ConfirmationResult, signInWithPhoneNumber, User } from "firebase/auth";
+import { auth } from "./@commons/utilities/firebase";
+
 function App() {
-  return <></>;
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const confirmationResult = useRef<ConfirmationResult | null>(null);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [isSMSSent, setIsSMSSent] = useState<boolean>(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const [code, setCode] = useState<string>("");
+  const { isVerified, _, recaptchaVerifier } = useReCaptcha(
+    buttonRef,
+    (response) => {
+      console.log(response);
+    },
+  );
+  const [user, setUser] = useState<User | null>(null);
+
+  const signIn = async () => {
+    if (!recaptchaVerifier) return;
+
+    try {
+      // sms 전송
+      confirmationResult.current = await signInWithPhoneNumber(
+        auth,
+        phoneNumber,
+        recaptchaVerifier,
+      );
+
+      setIsSMSSent(true);
+    } catch (e) {
+      setIsError(true);
+    }
+  };
+
+  return (
+    <>
+      <input
+        value={phoneNumber}
+        onChange={({ target: { value } }) => {
+          setPhoneNumber(value);
+        }}
+      />
+      <button ref={buttonRef} onClick={signIn}>
+        전화번호 인증
+      </button>
+      {isVerified && <p>인증완료</p>}
+      {isError && <p>메세지 전송 오류</p>}
+      {isSMSSent && (
+        <>
+          <input
+            value={code}
+            onChange={({ target: { value } }) => {
+              setCode(value);
+            }}
+            placeholder="인증번호 입력"
+          />
+          <button
+            onClick={() => {
+              confirmationResult.current?.confirm(code).then((result) => {
+                console.log(result.user);
+                setUser(auth.currentUser);
+              });
+            }}
+          >
+            인증번호 확인
+          </button>
+        </>
+      )}
+
+      {user && <p>{JSON.stringify(user.toJSON())}</p>}
+    </>
+  );
 }
 
 export default App;
