7a2fddbc7edd8317d5df4b0ea22a82c8ad5a3ea7
nakyoung98 <jay3925@cau.ac.kr>
2024-08-22 20:42:43 +0900

    Fix: focusAccessibilityStyles  보완
    레거시 브라우저 지원

--- eastar-fe/src/styles/accessibility.styles.ts
+++ eastar-fe/src/styles/accessibility.styles.ts
@@ -7,18 +7,18 @@ const focusStyles = css`
 
 export const focusAccessibilityStyles = css`
   /* 기본 포커스 스타일 */
-  :focus {
-    ${focusStyles}
-  }
-
-  /* 마우스 사용자를 위한 포커스 스타일 제거 */
-  :focus:not(:focus-visible) {
+  &:focus {
     outline: none;
-    box-shadow: none;
   }
 
-  /* 키보드 사용자를 위한 포커스 스타일 */
-  :focus-visible {
+  &:focus-visible:not(input) {
     ${focusStyles}
+  }
+
+  /* 래거시 브라우저를 위한 포커스 스타일 */
+  @supports not selector(:focus-visible) {
+    &:focus {
+      ${focusStyles}
+    }
   }
 `;
