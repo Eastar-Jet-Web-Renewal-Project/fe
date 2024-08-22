efa18da1583377c1e3a65633b7fb1bffea0ae227
nakyoung98 <jay3925@cau.ac.kr>
2024-08-22 12:43:08 +0900

    Feat: DesignComponentPage SingleLineInput 컴포넌트 추가

--- eastar-fe/src/domains/design/pages/DesignComponentPage.tsx
+++ eastar-fe/src/domains/design/pages/DesignComponentPage.tsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import Button from "../../../@commons/components/Default/Button/Button";
 import { ButtonVariant } from "../../../@commons/components/Default/Button/Button.styled";
+import SingleLineInput from "../../../@commons/components/Default/Input/SingleLineInput";
+import { SingleLineInputVariant } from "../../../@commons/components/Default/Input/SingleLineInput.styled";
 
 export default function DesignComponentPage() {
   const ButtonStyles: ButtonVariant[] = [
@@ -9,6 +12,16 @@ export default function DesignComponentPage() {
     "Outlined",
     "Text",
   ];
+  const SingleLineInputStyles: SingleLineInputVariant[] = [
+    "Filled",
+    "Outlined",
+    "Text",
+  ];
+
+  const [value, setValue] = useState<string>("");
+  const onChangeValue = (value: string) => {
+    setValue(value);
+  };
   return (
     <>
       <h1>Styled Component List Page</h1>
@@ -22,6 +35,19 @@ export default function DesignComponentPage() {
       ))}
       <h3>diabled</h3>
       <Button content="Button" disabled />
+      <h2>SingleLineInput</h2>
+      {SingleLineInputStyles.map((style) => (
+        <div key={style}>
+          <h3>{style}</h3>
+          <SingleLineInput
+            style={style}
+            value={value}
+            onChange={onChangeValue}
+          />
+        </div>
+      ))}
+      <h3>diabled</h3>
+      <SingleLineInput value={value} onChange={onChangeValue} disabled />
     </>
   );
 }
