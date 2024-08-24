bd3d58c5e89b5dc361f4b9983c3c03a616ec647c
nakyoung98 <jay3925@cau.ac.kr>
2024-08-25 01:33:52 +0900

    Feat: getFontSize 구현
     * getFontSize 함수는 주어진 타이포그래피 타입과 선택적인 크기에 따라 폰트 크기를 반환합니다.
     *
     * @param {TyphographyType} type - 타이포그래피 타입을 지정합니다.
     * @param {TyphographySize} [size] - 선택적으로 타이포그래피 크기를 지정합니다. 단, 지정하지 않거나 존재하지 않는 크기를 지정할 경우 기본 크기를 반환합니다.
     * @returns {string} - 주어진 타입과 크기에 해당하는 폰트 크기를 반환합니다.
     *
     * @example
     * const fontSize = getFontSize('Hero', '1');
     * // fontSize는 'Hero' 타입의 '1' 크기에 해당하는 폰트 크기를 포함합니다.

--- eastar-fe/src/@commons/utilities/style/textUtility.ts
+++ eastar-fe/src/@commons/utilities/style/textUtility.ts
@@ -30,3 +30,28 @@ export const getFontStyleCSS = (
   font-weight: ${getFontWeight(weight)};
   color: ${getColorWithTheme(color)};
 `;
+
+/**
+ * getFontSize 함수는 주어진 타이포그래피 타입과 선택적인 크기에 따라 폰트 크기를 반환합니다.
+ *
+ * @param {TyphographyType} type - 타이포그래피 타입을 지정합니다.
+ * @param {TyphographySize} [size] - 선택적으로 타이포그래피 크기를 지정합니다. 단, 지정하지 않거나 존재하지 않는 크기를 지정할 경우 기본 크기를 반환합니다.
+ * @returns {string} - 주어진 타입과 크기에 해당하는 폰트 크기를 반환합니다.
+ *
+ * @example
+ * const fontSize = getFontSize('Hero', '1');
+ * // fontSize는 'Hero' 타입의 '1' 크기에 해당하는 폰트 크기를 포함합니다.
+ */
+export const getFontSize = (
+  type: TyphographyType,
+  size?: TyphographySize,
+): string => {
+  const typeFontSize = fontSize[type];
+
+  if (size && size in typeFontSize) {
+    return typeFontSize[size as keyof typeof typeFontSize];
+  } else {
+    return typeFontSize[1];
+  }
+};
+
