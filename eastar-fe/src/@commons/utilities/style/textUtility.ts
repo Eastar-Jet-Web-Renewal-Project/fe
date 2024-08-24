7b7584be27c62b7f6c8bd00a9e0fabeed6b2cb03
nakyoung98 <jay3925@cau.ac.kr>
2024-08-25 02:34:04 +0900

    Fix: 철자 오타 수정
    Typhography -> Typography

--- eastar-fe/src/@commons/utilities/style/textUtility.ts
+++ eastar-fe/src/@commons/utilities/style/textUtility.ts
@@ -1,18 +1,18 @@
 import { css } from "styled-components";
 import { fontSize, fontWeight } from "../../../styles/font.styles";
 import {
-  TyphographySize,
-  TyphographyType,
-  TyphographyWeight,
+  TypographySize,
+  TypographyType,
+  TypographyWeight,
 } from "../../types/font";
 import { getColorWithTheme } from "./colorUtility";
 
 /**
  * getFontStyleCSS 함수는 주어진 타이포그래피 타입, 크기, 굵기 및 색상에 따라 CSS 스타일을 생성합니다.
  *
- * @param {TyphographyType} type - 타이포그래피 타입을 지정합니다.
- * @param {TyphographySize} [size] - 선택적으로 타이포그래피 크기를 지정합니다.
- * @param {TyphographyWeight} [weight] - 선택적으로 타이포그래피 굵기를 지정합니다.
+ * @param {TypographyType} type - 타이포그래피 타입을 지정합니다.
+ * @param {TypographySize} [size] - 선택적으로 타이포그래피 크기를 지정합니다.
+ * @param {TypographyWeight} [weight] - 선택적으로 타이포그래피 굵기를 지정합니다.
  * @param {string} [color] - 선택적으로 타이포그래피 색상을 지정합니다.
  * @returns {FlattenSimpleInterpolation} - 주어진 속성에 따라 생성된 CSS 스타일을 반환합니다.
  *
@@ -21,9 +21,9 @@ import { getColorWithTheme } from "./colorUtility";
  * // style은 주어진 속성에 따라 생성된 CSS 스타일을 포함합니다.
  */
 export const getFontStyleCSS = (
-  type: TyphographyType,
-  size?: TyphographySize,
-  weight?: TyphographyWeight,
+  type: TypographyType,
+  size?: TypographySize,
+  weight?: TypographyWeight,
   color?: string,
 ) => css`
   font-size: ${getFontSize(type, size)};
@@ -34,8 +34,8 @@ export const getFontStyleCSS = (
 /**
  * getFontSize 함수는 주어진 타이포그래피 타입과 선택적인 크기에 따라 폰트 크기를 반환합니다.
  *
- * @param {TyphographyType} type - 타이포그래피 타입을 지정합니다.
- * @param {TyphographySize} [size] - 선택적으로 타이포그래피 크기를 지정합니다. 단, 지정하지 않거나 존재하지 않는 크기를 지정할 경우 기본 크기를 반환합니다.
+ * @param {TypographyType} type - 타이포그래피 타입을 지정합니다.
+ * @param {TypographySize} [size] - 선택적으로 타이포그래피 크기를 지정합니다. 단, 지정하지 않거나 존재하지 않는 크기를 지정할 경우 기본 크기를 반환합니다.
  * @returns {string} - 주어진 타입과 크기에 해당하는 폰트 크기를 반환합니다.
  *
  * @example
@@ -43,8 +43,8 @@ export const getFontStyleCSS = (
  * // fontSize는 'Hero' 타입의 '1' 크기에 해당하는 폰트 크기를 포함합니다.
  */
 export const getFontSize = (
-  type: TyphographyType,
-  size?: TyphographySize,
+  type: TypographyType,
+  size?: TypographySize,
 ): string => {
   const typeFontSize = fontSize[type];
 
@@ -58,12 +58,12 @@ export const getFontSize = (
 /**
  * getFontWeight 함수는 주어진 타이포그래피 굵기에 따라 폰트 굵기를 반환합니다.
  *
- * @param {TyphographyWeight} [weight] - 선택적으로 타이포그래피 굵기를 지정합니다.
+ * @param {TypographyWeight} [weight] - 선택적으로 타이포그래피 굵기를 지정합니다.
  * @returns {number} - 주어진 굵기에 해당하는 폰트 굵기를 반환합니다. 만약 굵기가 지정되지 않았거나 유효하지 않으면 기본 굵기(normal)를 반환합니다.
  *
  * @example
  * const fontWeight = getFontWeight('bold');
  * // fontWeight는 'bold'에 해당하는 폰트 굵기를 포함합니다.
  */
-export const getFontWeight = (weight?: TyphographyWeight): number =>
+export const getFontWeight = (weight?: TypographyWeight): number =>
   weight && weight in fontWeight ? fontWeight[weight] : fontWeight.normal;
