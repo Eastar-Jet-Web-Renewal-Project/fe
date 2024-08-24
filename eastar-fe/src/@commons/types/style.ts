f52ebdcc3300809860e301f95f0fe47c040c9306
nakyoung98 <jay3925@cau.ac.kr>
2024-08-25 01:44:13 +0900

    Chore: Theme 관련 type 추가

--- eastar-fe/src/@commons/types/style.ts
+++ eastar-fe/src/@commons/types/style.ts
@@ -1,4 +1,5 @@
 import { RuleSet } from "styled-components";
+import theme from "../../styles/Theme/light.theme";
 
 // Disabled 포함
 export type Variants<G extends string> = {
@@ -6,3 +7,5 @@ export type Variants<G extends string> = {
 } & {
   Disabled: RuleSet<object>;
 };
+
+export type Theme = keyof typeof theme;
