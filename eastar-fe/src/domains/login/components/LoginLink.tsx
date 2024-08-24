182df62cb2fe3d94d0016eaddb73b95bff792331
nakyoung98 <jay3925@cau.ac.kr>
2024-08-24 16:37:00 +0900

    Feat: Login Page 용 Link 컴포넌트 디자인

---
+++ eastar-fe/src/domains/login/components/LoginLink.tsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import theme from "../../../styles/Theme/light.theme";
+
+export const LoginLink = styled(Link)`
+  color: ${theme.link};
+`;
