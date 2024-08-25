e2991cb799ee3d121b0712db9cd707b5eac5a4b3
nakyoung98 <jay3925@cau.ac.kr>
2024-08-26 04:20:09 +0900

    Feat: SVG 컴포넌트 export 전용 파일 생성

---
+++ eastar-fe/src/@commons/components/Default/Images/SvgComponents.tsx
@@ -0,0 +1,18 @@
+import Logo from "@assets/logos/logo.svg?react";
+
+//SNS
+import Google from "@assets/sns/google.svg?react";
+import Facebook from "@assets/sns/facebook.svg?react";
+import Kakao from "@assets/sns/kakao.svg?react";
+import Apple from "@assets/sns/apple.svg?react";
+
+export default {
+  Logo,
+
+  SNS: {
+    Google,
+    Facebook,
+    Kakao,
+    Apple,
+  },
+};
