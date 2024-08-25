48d2637d2f729ee268d13a67e9c25c9da77822fd
nakyoung98 <jay3925@cau.ac.kr>
2024-08-26 04:18:56 +0900

    Chore: vite config에 svgr 플러그인 추가 및 path 라이브러리 설치

--- eastar-fe/vite.config.ts
+++ eastar-fe/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: [
+      {
+        find: "@commons",
+        replacement: path.resolve(__dirname, "src/@commons"),
+      },
+      { find: "@domains", replacement: path.resolve(__dirname, "src/domains") },
+      { find: "@assets", replacement: path.resolve(__dirname, "src/assets") },
+      { find: "@styles", replacement: path.resolve(__dirname, "src/styles") },
+    ],
+  },
+});
