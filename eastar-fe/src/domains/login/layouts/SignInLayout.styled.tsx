85eaa6014c8124786b110ae09faef9718faee7bf
nakyoung98 <jay3925@cau.ac.kr>
2024-08-26 10:30:05 +0900

    Feat: SigninLayout 구현

--- eastar-fe/src/domains/login/layouts/SignInLayout.styled.tsx
+++ eastar-fe/src/domains/login/layouts/SignInLayout.styled.tsx
@@ -1,21 +1,37 @@
 import styled from "styled-components";
 
 export const SignInLayoutContainer = styled.div`
+  margin: 0 auto;
+  padding: 120px 20px;
+
+  width: 100%;
+  max-width: 320px;
+  height: 100%;
+
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 100%;
-  height: 100%;
-  padding: 0 20px;
 `;
 
 export const SignInForm = styled.form`
+  width: 100%;
+
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 15px;
+`;
+
+export const SignInLayoutDivider = styled.div`
   width: 100%;
-  max-width: 320px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 14px;
+
+  & > *:nth-child(2) {
+    flex-shrink: 0;
+  }
 `;
--- eastar-fe/src/domains/login/layouts/SignInLayout.tsx
+++ eastar-fe/src/domains/login/layouts/SignInLayout.tsx
@@ -1,9 +1,67 @@
-import { Link } from "react-router-dom";
-import Button from "../../../@commons/components/Default/Button/Button";
-import SingleLineInput from "../../../@commons/components/Default/Input/SingleLineInput";
-import { SignInForm, SignInLayoutContainer } from "./SignInLayout.styled";
+import Button from "@commons/components/Default/Button/Button";
+import SingleLineInput from "@commons/components/Default/Input/SingleLineInput";
+import {
+  SignInForm,
+  SignInLayoutContainer,
+  SignInLayoutDivider,
+} from "./SignInLayout.styled";
+import DividerList from "@commons/components/Default/Divider/DividerList";
+import Divider from "@commons/components/Default/Divider/Divider";
+import Typography from "@commons/components/Default/Typography/Typography";
+import theme from "@styles/Theme/light.theme";
+import Spacer from "@commons/components/Default/Spacer";
+import S from "@commons/components/Default/Images/SvgComponents";
+import Row from "@commons/components/Default/Layer/Row";
+import { LoginLink } from "../components/LoginLink";
 
 export default function SignInLayout() {
   return (
+    <SignInLayoutContainer>
+      <S.Logo />
+      <Typography type="subHeading" size={1} color="onSurfaceContainer">
+        기분 좋은 만남.
+      </Typography>
+      <Spacer size={48} />
+      <SignInForm>
+        <SingleLineInput value="" onChange={() => {}} placeholder="아이디" />
+        <Spacer size={16} />
+        <SingleLineInput
+          value=""
+          onChange={() => {}}
+          placeholder="비밀번호"
+          type="password"
+        />
+        <Spacer size={20} />
+        <Button content="로그인" style="Filled" />
+      </SignInForm>
+      <Spacer size={32} />
+      <DividerList>
+        {/* //TODO: Link 연결 */}
+        <LoginLink to={""}>아이디 찾기</LoginLink>
+        <LoginLink to={""}>비밀번호 찾기</LoginLink>
+      </DividerList>
+      <Spacer size={60} />
+      <SignInLayoutDivider>
+        <Divider />
+        <Typography type="body" size={4} color={theme.hint}>
+          또는
+        </Typography>
+        <Divider />
+      </SignInLayoutDivider>
+      <Spacer size={22} />
+      <Row gap={16}>
+        <S.SNS.Google />
+        <S.SNS.Kakao />
+        <S.SNS.Facebook />
+        <S.SNS.Apple />
+      </Row>
+      <Spacer size={32} />
+      <DividerList gap={8}>
+        {/* //TODO: Link 연결 */}
+        <LoginLink to={""}>회원가입</LoginLink>
+        <LoginLink to={""}>이메일 로그인</LoginLink>
+        <LoginLink to={""}>문의하기</LoginLink>
+      </DividerList>
+    </SignInLayoutContainer>
   );
 }
