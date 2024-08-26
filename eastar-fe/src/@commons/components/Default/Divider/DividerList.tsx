fba6068b0c3e4a1b84b89083a77b7855eee4c1d3
nakyoung98 <jay3925@cau.ac.kr>
2024-08-27 00:42:05 +0900

    Fix: styled 상속을 위한 className 프롭 추가

--- eastar-fe/src/@commons/components/Default/Divider/DividerList.tsx
+++ eastar-fe/src/@commons/components/Default/Divider/DividerList.tsx
@@ -5,12 +5,14 @@ type DividerListProps = {
   children: React.ReactNode;
   isVertical?: boolean;
   gap?: number;
+  className?: string;
 };
 
 export default function DividerList({
   children,
   isVertical,
   gap,
+  className,
 }: DividerListProps) {
   // null과 undefined를 제외하고, 유효한 React 엘리먼트인지 확인
   const childrenArray = React.Children.toArray(children).filter(
@@ -18,7 +20,7 @@ export default function DividerList({
       child !== null && child !== undefined && React.isValidElement(child),
   );
   return (
-    <StyledDividerList $isVertical={isVertical}>
+    <StyledDividerList $isVertical={isVertical} className={className}>
       {childrenArray.map((content) => (
         <StyledDivider $isVertical={isVertical} $gap={gap} key={content.key}>
           {content}
