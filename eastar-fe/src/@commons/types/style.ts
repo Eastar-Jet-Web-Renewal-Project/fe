d3bcc6e9017a0ed2915be6587f9d86fc070b736d
nakyoung98 <jay3925@cau.ac.kr>
2024-08-26 09:27:16 +0900

    Feat: 특정 prefix 삭제 Type 구현 및 $삭제 Type 확장

--- eastar-fe/src/@commons/types/style.ts
+++ eastar-fe/src/@commons/types/style.ts
@@ -1,5 +1,6 @@
 import { RuleSet } from "styled-components";
 import theme from "../../styles/Theme/light.theme";
+import { RemovePrefix } from "./UtilType";
 
 // Disabled 포함
 export type Variants<G extends string> = {
@@ -9,3 +10,9 @@ export type Variants<G extends string> = {
 };
 
 export type Theme = keyof typeof theme;
+
+export type Remove$Prefix<
+  STYLED_PROP extends {
+    [key: string]: any;
+  },
+> = RemovePrefix<STYLED_PROP, "$">;
