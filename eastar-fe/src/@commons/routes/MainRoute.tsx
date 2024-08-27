4db3b7bdb6391cd47795ab0c6831adbf53cca398
nakyoung98 <jay3925@cau.ac.kr>
2024-08-27 16:05:01 +0900

    Chore: domain 이름 변경 (login -> sign-in)

--- eastar-fe/src/@commons/routes/MainRoute.tsx
+++ eastar-fe/src/@commons/routes/MainRoute.tsx
@@ -1,7 +1,7 @@
 import { Route } from "react-router-dom";
 import DesignSystem from "../../domains/design/pages/DesignSystem";
 import DesignComponentPage from "../../domains/design/pages/DesignComponentPage";
-import SignInPage from "../../domains/login/pages/SignInPage";
+import SignInPage from "../../domains/sign-in/pages/SignInPage";
 
 export const mainRouter = (
   <Route path="/">
