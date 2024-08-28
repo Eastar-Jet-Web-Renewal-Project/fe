d470aefc1468c59f1f87355fa07b8e97f6dcd483
nakyoung98 <jay3925@cau.ac.kr>
2024-08-28 21:15:09 +0900

    Feat: DropdownTrigger 구현

---
+++ eastar-fe/src/@commons/components/Portal/Dropdown/DropdownTrigger/DropdownTrigger.styled.tsx
@@ -0,0 +1,42 @@
+import { DefaultStyleFormElement } from "@commons/components/Default/Form/DefaultFormElementStyle";
+import { Variants } from "@commons/types/style";
+import theme from "@styles/Theme/light.theme";
+import styled, { css } from "styled-components";
+
+export type DropdownTriggerVariant = "Filled";
+
+export const DropdownTriggerStyles: Variants<DropdownTriggerVariant> = {
+  Filled: css`
+    background-color: ${theme.surfaceContainer};
+    color: ${theme.onSurfaceContainer};
+  `,
+  Disabled: css`
+    background-color: ${theme.surfaceContainerHighest};
+    color: ${theme.onSurfaceContainerHighest};
+    &::placeholder {
+      color: ${theme.onSurfaceContainerHighest};
+    }
+    cursor: not-allowed;
+  `,
+};
+
+export type StyledDropdownTriggerProps = {
+  $variant: DropdownTriggerVariant;
+  $isDisabled?: boolean;
+};
+
+export const StyledDropdownTrigger = styled(
+  DefaultStyleFormElement,
+).attrs<StyledDropdownTriggerProps>(() => ({
+  as: "p",
+}))`
+  width: 100%;
+  justify-content: start;
+
+  cursor: pointer;
+
+  ${(props) => DropdownTriggerStyles[props.$variant]}
+
+  //disabled일 경우 위 속성을 모두 무효화
+  ${(props) => props.$isDisabled && DropdownTriggerStyles["Disabled"]}
+`;
---
+++ eastar-fe/src/@commons/components/Portal/Dropdown/DropdownTrigger/DropdownTrigger.tsx
@@ -0,0 +1,24 @@
+import {
+  DropdownTriggerVariant,
+  StyledDropdownTrigger,
+} from "./DropdownTrigger.styled";
+
+type DropdownTriggerProps = {
+  content?: string;
+  style?: DropdownTriggerVariant;
+  disabled?: boolean;
+  placeholder?: string;
+};
+
+export default function DropdownTrigger({
+  content,
+  style,
+  disabled,
+  placeholder,
+}: DropdownTriggerProps) {
+  return (
+    <StyledDropdownTrigger $variant={style || "Filled"} $isDisabled={disabled}>
+      {content || placeholder || "이 곳을 눌러 가능한 값을 얻을 수 있습니다"}
+    </StyledDropdownTrigger>
+  );
+}
