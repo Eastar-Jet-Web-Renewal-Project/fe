ae4864797c6c948567f138b8d3d97612e4acee24
nakyoung98 <jay3925@cau.ac.kr>
2024-08-28 16:59:31 +0900

    Chore: CreateFlight 및 FlightDetail Layout&Page 생성

---
+++ eastar-fe/src/domains/admin/manage-flight/layouts/CreateFlightLayout.tsx
@@ -0,0 +1,3 @@
+export default function CreateFlightLayout() {
+  return <div>CreateFlightLayout</div>;
+}
---
+++ eastar-fe/src/domains/admin/manage-flight/pages/FlightDetailPage.tsx
@@ -0,0 +1,3 @@
+export default function FlightDetailPage() {
+  return <div>FlightDetailPage</div>;
+}
