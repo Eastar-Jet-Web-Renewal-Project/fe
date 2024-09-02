36e5995c9f31be5ca5cecc837e8f2ecf77c1781d
nakyoung98 <jay3925@cau.ac.kr>
2024-09-03 03:46:13 +0900

    Fix: SingleLineInput 컴포넌트 수정
    - options prop 추가: 추가적인 HTML 속성을 전달할 수 있게 됨

--- eastar-fe/src/@commons/components/Default/Form/Input/SingleLineInput.tsx
+++ eastar-fe/src/@commons/components/Default/Form/Input/SingleLineInput.tsx
@@ -8,22 +8,40 @@ import {
 type SingleLineInputProps = {
   value: string;
   onChange: (value: string) => void;
-  style?: DefaultStyleFormElementVariant;
   disabled?: boolean;
   placeholder?: string;
+  style?: DefaultStyleFormElementVariant;
   type?: React.HTMLInputTypeAttribute;
+  options?: Omit<
+    React.HTMLAttributes<HTMLInputElement>,
+    "value" | "onChange" | "placeholder" | "type"
+  >;
 };
 
 /**
- * SingleLineInput 컴포넌트는 한 줄 텍스트 입력을 위한 입력 필드를 렌더링합니다.
+ * 단일 라인의 텍스트 입력 필드를 렌더링합니다.
  *
- * @example
+ * Props:
+ * - value: string - 입력 필드의 초기 값
+ * - onChange: (value: string) => void - 입력 값이 변경될 때 호출되는 콜백 함수
+ * - disabled?: boolean - 입력 필드를 비활성화할지 여부 (선택 사항)
+ * - placeholder?: string - 입력 필드의 플레이스홀더 텍스트 (선택 사항)
+ * - style?: DefaultStyleFormElementVariant - 입력 필드의 스타일 (선택 사항)
+ * - type?: React.HTMLInputTypeAttribute - 입력 필드의 타입 (예: 'text', 'password') (선택 사항)
+ * - options?: Omit<React.HTMLAttributes<HTMLInputElement>, "value" | "onChange" | "placeholder" | "type"> - 추가적인 HTML 속성 (선택 사항)
+ *
+ * 사용 예:
+ * ```tsx
  * <SingleLineInput
  *   value={inputValue}
  *   onChange={handleInputChange}
- *   style="Filled"
- *   placeholder="Enter text..."
+ *   placeholder="Enter text here"
+ *   disabled={false}
+ *   style={customStyle}
+ *   type="text"
+ *   options={{ maxLength: 50 }}
  * />
+ * ```
  */
 export default function SingleLineInput({
   value,
@@ -32,6 +50,7 @@ export default function SingleLineInput({
   disabled,
   placeholder,
   type,
+  options,
 }: SingleLineInputProps) {
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (disabled) return;
@@ -47,6 +66,7 @@ export default function SingleLineInput({
         onChange={handleChange}
         value={value}
         placeholder={disabled ? "입력하실 수 없습니다" : placeholder}
+        {...options}
       />
     </DefaultStyleFormElement>
   );
