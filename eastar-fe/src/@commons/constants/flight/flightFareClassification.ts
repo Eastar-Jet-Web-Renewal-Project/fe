7c7e72a2616554f25e6b6e163d458d8592a954cc
nakyoung98 <jay3925@cau.ac.kr>
2024-09-03 03:46:35 +0900

    Chore: flightFareClassification Option 상수 추가

---
+++ eastar-fe/src/@commons/constants/flight/flightFareClassification.ts
@@ -0,0 +1,7 @@
+import { Option } from "@commons/types/commons";
+
+export const flightFareClassification: Option[] = [
+  { label: "정상 운임", value: "Normal Fare" },
+  { label: "할인 운임", value: "Discount Fare" },
+  { label: "특가 운임", value: "Special Fare" },
+];
