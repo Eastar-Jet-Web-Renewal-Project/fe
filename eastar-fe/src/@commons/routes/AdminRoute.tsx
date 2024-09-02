48a20843d670f35610762cc865d2b39d48d9310f
nakyoung98 <jay3925@cau.ac.kr>
2024-09-02 23:35:36 +0900

    Fix: admin 내 경로 대폭 변경
    flight route 신설
    search, create, :id 하위 경로 추가

--- eastar-fe/src/@commons/routes/AdminRoute.tsx
+++ eastar-fe/src/@commons/routes/AdminRoute.tsx
@@ -1,11 +1,12 @@
-import CreateFlightPage from "@domains/admin/manage-flight/pages/CreateFlightPage";
-import FlightDetailPage from "@domains/admin/manage-flight/pages/FlightDetailPage";
+import CreateFlightPage from "@domains/admin/create-flight/pages/CreateFlightPage";
+import FlightDetailPage from "@domains/admin/flight-detail/pages/FlightDetailPage";
 import ManageFlightPage from "@domains/admin/manage-flight/pages/ManageFlightPage";
 import { Route } from "react-router-dom";
 
 export const adminRouter = (
   <Route path="/admin">
-    <Route path="manage-flight" element={<ManageFlightPage />}>
+    <Route path="flight">
+      <Route path="search" element={<ManageFlightPage />} />
       <Route path="create" element={<CreateFlightPage />} />
       <Route path=":id" element={<FlightDetailPage />} />
     </Route>
