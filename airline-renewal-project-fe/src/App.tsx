e5597191a41ac61ff2475046aea078df20ac1d1a
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-12-30 17:11:57 +0000

    Chore: queryClient 폴더 분리

--- airline-renewal-project-fe/src/App.tsx
+++ airline-renewal-project-fe/src/App.tsx
@@ -1,15 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import Router from "@routes";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5,
-    },
-  },
-});
+import { queryClient } from "@lib/tanstack-query";
 
 function App() {
   return (
